765f1b970500a82b2932820237ad695f99f1e2ca
John Woo <johnnyworker1012@gmail.com>
2019-10-18 17:17:05 +1100

    bugfix

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ def pad_sents(sents, pad_token):
     ### YOUR CODE HERE (~6 Lines)
     sent_lens = [len(sent) for sent in sents]
     max_len = max(sent_lens)
-    sents_padded = [(sents[i] + [pad_token * (max_len - sent_lens[i])]) for i in range(len(sents))]
+    sents_padded = [sents[i] + [pad_token] * (max_len - sent_lens[i]) for i in range(len(sents))]
     ### END YOUR CODE
 
     return sents_padded
